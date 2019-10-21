b22a7a40df488ac2d48f95391422af6136757140
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-21 13:15:54 +0700

    Menambahkan halaman Home dan Mahasiswa

---
+++ application/views/Mahasiswa/index.php
@@ -0,0 +1,17 @@
+<div class="container">
+
+      <div class="row">
+            <div class="col-md-6">
+                  <h3>Daftar Mahasiswa</h3>
+                  <ul class="list-group">
+                  <?php foreach( $mahasiswa as $mhs) : ?>
+                        <li class="list-group-item"><?= $mhs['nama']; ?></li>
+<?php endforeach; ?>
+                  </ul>
+            </div>
+      
+      </div>
+
+
+
+</div>
